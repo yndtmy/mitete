ae2701652c14f30433b09ed814ec6eb82425a5e6
yndtmy <tmyt824@gmail.com>
2019-09-23 20:23:20 +0900

    ログイン追加

---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCZU6tMt-YXem-zS7JJbKkv1c_aZGqFWck",
+    authDomain: "mitete-3804a.firebaseapp.com",
+    databaseURL: "https://mitete-3804a.firebaseio.com",
+    projectId: "mitete-3804a",
+    storageBucket: "",
+    messagingSenderId: "788696255230",
+    appId: "1:788696255230:web:0c27816d405215e7c373af"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
